69d24527b7ce38db9721fff3a379777ff5f23d2a
RameezSTC <ramakhdoomi@stc.in>
2024-01-04 11:43:52 +0530

    -

--- app/app.mjs
+++ app/app.mjs
@@ -1,7 +1,7 @@
 const express = require('express')
 const serverless = require('serverless-http')
 const app = express()
-app.get('hello', (req,res) => res.send('Hi Im Hit First Time'))
+app.get('hello', (req,res) => res.send('Hi Im Hit First Time 1'))
 app.listen(4210, () => console.log("Listing on Port 4210"))
 
 module.exports.handler = serverless(app)
